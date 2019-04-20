5d61775d34d0528e59410e822f5853445152f8a3
larry adams <ladams1776@yahoo.com>
2019-04-20 08:39:44 -0400

    Add the Shuffle switch
    Need to have a more random deck set.

--- src/main.js
+++ src/main.js
@@ -17,10 +17,10 @@ const storeStructure = {
 }
 
 
-
+// utility function
 function createDeck() {
-    const suits = ["hearts", "diamonds", "spades", "clubs"]
-    const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    const suits = ["hearts", "diamonds", "spades", "clubs"];
+    const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
     const deck = [];
     for (suit of suits) {
@@ -45,13 +45,14 @@ const reducer = (state, action) => {
 
 
     switch(action.type) {
-        case 'DEAL':
+        case 'DEAL': {
             const copy = [...state.deck]
             return {
                 deck: copy,
                 dealer: [copy.pop(), copy.pop()],
-                dealer: [copy.pop(), copy.pop()],
+                player: [copy.pop(), copy.pop()],
             }
+        }
         case 'HIT': {
             const copy = [...state.deck];
             return {
@@ -61,9 +62,49 @@ const reducer = (state, action) => {
             }
 
         }
+        case 'SHUFFLE': {
+            const copy = [...state.deck]
+
+            // shuffle the deck with Fisher-Yates algorithm (swap every index of an array with a randomly-chosen index).
+            return {
+                deck: shuffleDeck(copy),
+                dealer: state.dealer,
+                player: state.player
+            };
+        }
         default: {
-            return state
+            return state;
         }
 
     }
 }
+
+function shuffleDeck(copy) {
+    for (let index in copy) {
+        let swapIndex = Math.floor(Math.random() * copy.length);
+        let tempCard = copy[swapIndex];
+        copy[swapIndex] = copy[index];
+        copy[index] = tempCard;
+    }
+
+    return copy;
+}
+
+
+
+
+const store = redux.createStore(reducer);
+
+store.subscribe(() => {
+    const state = store.getState()
+    console.log('deck: ', state.deck.length);
+    console.log('dealer: ', state.dealer);
+    console.log('player: ', state.player);
+    console.log();
+});
+
+
+store.dispatch({type: 'SHUFFLE'});
+store.dispatch({type: 'DEAL'});
+store.dispatch({type: 'HIT'});
+store.dispatch({type: 'HIT'});
